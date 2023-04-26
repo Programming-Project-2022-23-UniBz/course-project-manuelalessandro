e46810fe894e756964146598fee10c7fa510bdc2
Manuel Villotti <mvillotti@unib.it>
2023-04-26 09:02:11 +0200

    Remove exitonclose on login/register

--- src/main/java/LoginGUI/RegisterFrame.java
+++ src/main/java/LoginGUI/RegisterFrame.java
@@ -34,7 +34,6 @@ public class RegisterFrame extends JFrame{
         y = 125;
         
         //setting panel
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 500);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
